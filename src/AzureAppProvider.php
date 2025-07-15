8e3a4ee70af29138b9cb706721028772803245b8
stemple231 <stephen.temple@e-track.com>
2025-07-15 09:09:52 +0100

    version 1

--- src/AzureAppProvider.php
+++ src/AzureAppProvider.php
@@ -2,35 +2,72 @@
 
 namespace EtrackSystems\SocialiteAzureApp;
 
+use GuzzleHttp\RequestOptions;
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 
 class AzureAppProvider extends AbstractProvider
 {
-    protected $scopes = ['openid', 'profile', 'email'];
+    protected $scopes = ['api://2b54c30b-2128-4e85-8061-d2170abfc8bc/.default', 'openid'];
+
+    protected $scopeSeparator = ' ';
+
+
+    public static function additionalConfigKeys()
+    {
+        return ['tenant_id'];
+    }
+
+    public function getAccessToken($code)
+    {
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            RequestOptions::FORM_PARAMS => $this->getTokenFields($code),
+        ]);
+
+        $this->credentialsResponseBody = json_decode((string)$response->getBody(), true);
+
+        return $this->parseAccessToken($response->getBody());
+    }
+
+    protected function getBaseUrl(): string
+    {
+        return 'https://login.microsoftonline.com/' . $this->getConfig('tenant_id');
+    }
+
+    public function getAccessTokenResponse($code)
+    {
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            RequestOptions::HEADERS     => ['Accept' => 'application/json'],
+            RequestOptions::FORM_PARAMS => $this->getTokenFields($code),
+            //RequestOptions::PROXY       => $this->getConfig('proxy'),
+        ]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
 
     protected function getAuthUrl($state)
     {
-        return $this->buildAuthUrlFromBase(
-            "https://login.microsoftonline.com/" .
-            $this->getConfig('tenant_id') .
-            "/oauth2/v2.0/authorize",
-            $state
-        );
+        return $this->buildAuthUrlFromBase($this->getBaseUrl() . '/oauth2/v2.0/authorize', $state);
     }
 
     protected function getTokenUrl()
     {
-        return "https://login.microsoftonline.com/" . $this->getConfig('tenant_id') . "/oauth2/v2.0/token";
+        return $this->getBaseUrl() . '/oauth2/v2.0/token';
     }
 
     protected function getUserByToken($token)
     {
-        $response = $this->getHttpClient()->get(
-            'https://graph.microsoft.com/v1.0/me',
-            ['headers' => ['Authorization' => 'Bearer ' . $token]]
-        );
+        $parts = explode('.', $token);
+        if (count($parts) !== 3) {
+            throw new \Exception('Invalid JWT token');
+        }
 
-        return json_decode($response->getBody()->getContents(), true);
+        $payload = json_decode(base64_decode(strtr($parts[1], '-_', '+/')), true);
+
+        return [
+            'id' => $payload['oid'] ?? $payload['sub'] ?? null,
+            'name' => $payload['name'] ?? null,
+            'email' => $payload['upn'] ?? null,
+        ];
     }
 
     protected function mapUserToObject(array $user)
@@ -39,15 +76,8 @@ class AzureAppProvider extends AbstractProvider
             'id'       => $user['id'],
             'nickname' => null,
             'name'     => $user['displayName'] ?? null,
-            'email'    => $user['mail'] ?? $user['userPrincipalName'] ?? null,
+            'email'    => $user['mail'] ?? $user['userPrincipalName'] ?? $user['upn'] ?? null,
             'avatar'   => null,
-        ]);
-    }
-
-    protected function getTokenFields($code)
-    {
-        return array_merge(parent::getTokenFields($code), [
-            'scope' => 'api://' . $this->getConfig('client_id') . '/.default',
         ]);
     }
 }
--- src/README.md
+++ src/README.md
@@ -6,6 +6,21 @@ composer require etracksystems/socialite-azure-app
 
 ## Installation & Basic Usage
 
+### Azure
+
+you'll require two apps registered with Microsoft Entra ID. The first acts as the '*login gateway*' where you can apply
+policies, and the second acts as the actual '*eTrack resource*' the login app is requesting access for. This is due to
+the nuance of Azure, where a registered app cannot request scope access to itself.
+
+Expose an API endpoint on the second '*resource*' app. The actual URI doesnt matter, its just to register an endpoint for
+the first app to request access for. Then in the first '*login*' app under ```API Permissions``` add the URI endpoint
+you just created in the second '*resource*' app.
+
+<span style="color:red;">Note: because we're requesting a token directly for the app, we only get basic user info back. Such a caveat is the users
+email. We cant actually get this as we dont have access to Graph, and its not returned in the access token. We do have
+the upn (user principle name) which typically is the users primary email address, but not always. This package makes
+the assumption that is, if this is not the case for your environment then the email address will likely be wrong.</span>
+
 Please see the [Base Installation Guide](https://socialiteproviders.com/usage/), then follow the provider specific instructions below.
 
 ### Add configuration to your `config/services.php`
